cc3822c133e4b8e7fff9f25a55fecf0c6153c6aa
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-01-14 16:25:20 -0300

    Melhoria na exibição do tabuleiro

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -27,8 +27,18 @@ public class ChessMatch {
 	}
 
 	private void initialSetup() {
-		placeNewPiece('b', 6, new Rook(board, Color.BRANCO));
-		placeNewPiece('e', 8, new King(board, Color.PRETO));
-		placeNewPiece('e', 1, new King(board, Color.BRANCO));
+		placeNewPiece('c', 1, new Rook(board, Color.BRANCO));
+		placeNewPiece('c', 2, new Rook(board, Color.BRANCO));
+		placeNewPiece('d', 2, new Rook(board, Color.BRANCO));
+		placeNewPiece('e', 2, new Rook(board, Color.BRANCO));
+		placeNewPiece('e', 1, new Rook(board, Color.BRANCO));
+		placeNewPiece('d', 1, new King(board, Color.BRANCO));
+
+		placeNewPiece('c', 7, new Rook(board, Color.PRETO));
+		placeNewPiece('c', 8, new Rook(board, Color.PRETO));
+		placeNewPiece('d', 7, new Rook(board, Color.PRETO));
+		placeNewPiece('e', 7, new Rook(board, Color.PRETO));
+		placeNewPiece('e', 8, new Rook(board, Color.PRETO));
+		placeNewPiece('d', 8, new King(board, Color.PRETO));
 	}
 }
