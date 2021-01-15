69ad372bfcbdad98f4510034296876ac41e8eb9a
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-01-15 16:11:17 -0300

    Lógica de xequemate

--- src/application/UI.java
+++ src/application/UI.java
@@ -56,9 +56,14 @@ public class UI {
 		System.out.println();
 		printCapturedPiece(captured);
 		System.out.println("Turn: " + chessMatch.getTurn());
-		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
-		if (chessMatch.getCheck()) {
-			System.out.println("CHECK!");
+		if (!chessMatch.getCheckMate()) {
+			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+			if (chessMatch.getCheck()) {
+				System.out.println("CHECK!");
+			}
+		} else {
+			System.out.println("CHECKMATE!");
+			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
 		}
 	}
 
