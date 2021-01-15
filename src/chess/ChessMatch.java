1e500f3dc9346f073d0ffd33bc688559d6740be2
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-01-15 16:20:29 -0300

    Contagem de movimento das peças

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -82,7 +82,8 @@ public class ChessMatch {
 	}
 
 	private Piece makeMove(Position source, Position target) {
-		Piece p = board.removePiece(source);
+		ChessPiece p = (ChessPiece) board.removePiece(source);
+		p.increaseMoveCount();
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
 		if (capturedPiece != null) {
@@ -93,7 +94,8 @@ public class ChessMatch {
 	}
 
 	private void undoMove(Position source, Position target, Piece capturedPiece) {
-		Piece p = board.removePiece(target);
+		ChessPiece p = (ChessPiece) board.removePiece(target);
+		p.decreaseMoveCount();
 		board.placePiece(p, source);
 		if (capturedPiece != null) {
 			board.placePiece(capturedPiece, target);
@@ -188,7 +190,7 @@ public class ChessMatch {
 		placeNewPiece('a', 1, new Rook(board, Color.WHITE));
 		placeNewPiece('h', 1, new Rook(board, Color.WHITE));
 		placeNewPiece('d', 1, new King(board, Color.WHITE));
-		
+
 		placeNewPiece('d', 8, new King(board, Color.BLACK));
 	}
 }
